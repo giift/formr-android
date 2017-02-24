1e4f42d8fb68d12ecea514f4a9dfb48dcf98d4a4
giift <vieony.dacosta@giift.com>
2017-02-24 12:43:45 +0800

    GAU-413 Bitrise failing test

--- formr/src/androidTest/java/com/giift/formr/field/TextTest.java
+++ formr/src/androidTest/java/com/giift/formr/field/TextTest.java
@@ -56,10 +56,13 @@ public class TextTest {
     onView(withId(R.id.text)).check(matches(isDisplayed()));
     Matcher<View> linearLayout = allOf(isAssignableFrom(LinearLayout.class), withParent(withId(R.id.text)));
     textInputLayout_ = allOf(isAssignableFrom(TextInputLayout.class), withParent(linearLayout));
+    onView(textInputLayout_).perform(closeSoftKeyboard());
     onView(textInputLayout_).perform(scrollTo());
     Matcher<View> frameLayoutLayout = allOf(isAssignableFrom(FrameLayout.class), withParent(textInputLayout_));
+    onView(frameLayoutLayout).perform(closeSoftKeyboard());
     onView(frameLayoutLayout).perform(scrollTo());
     textInputEditText_ = allOf(isAssignableFrom(TextInputEditText.class), withParent(frameLayoutLayout));
+    onView(textInputEditText_).perform(closeSoftKeyboard());
     onView(textInputEditText_).perform(scrollTo());
   }
 
